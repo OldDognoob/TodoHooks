702e8f959a4a8b927b42a4f932dabe99fc3cf736
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-02 22:13:29 +0200

    wip-todoForm

--- my-list/src/components/TodoForm.js
+++ my-list/src/components/TodoForm.js
@@ -4,13 +4,19 @@ function TodoForm(props) {
 
     const[input, setInput] = useState('');
 
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
+
     const handleSubmit = e => {
         e.preventDefault();
 
         props.onSubmit({
+            // generates a random number at ten thousands
             id: Math.floor(Math.random() * 10000),
             text: input
         });
+        // setting the input in an empty string
         setInput('');
     }
 
@@ -22,6 +28,9 @@ function TodoForm(props) {
       value={input}
       name='text'
       className='todo-input'
+      //insert handleChange and create a function
+      onChange={handleChange}
+      
       />
       <button className='todo-button'>Add todo</button>
   </form>
