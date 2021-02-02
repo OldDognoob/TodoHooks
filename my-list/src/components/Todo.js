33bae1a62892018c7097268af88265a897c8e389
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-02-02 22:52:18 +0200

    todo

---
+++ my-list/src/components/Todo.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import TodoForm from './TodoForm';
+import { RiCloseCircleLine} from 'react-icons/ri';
+import {TiEdit } from 'react-icons/ti';
+
+function Todo() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Todo;
